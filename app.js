f307633baa1fa24903a10aefa356d790cfd95244
morita <morita21@gmail.com>
2023-02-19 14:34:23 +0900

    2022-12-11

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const roadSize = 300;
+const roadTileNum = 5;
+const manInitZ = 0;
 
 // =======================================================
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x111111);
+// Fogを使うと遠くのライトが薄くなってしまう
+// scene.fog = new THREE.FogExp2( 0x000000, 0.0025 );
 
 const camera1 = new THREE.PerspectiveCamera(
   75,
@@ -22,10 +26,17 @@ const camera2 = new THREE.PerspectiveCamera(
 let anime = true;
 updateIsPlayingLabel()
 
+let renderer2Size = 0.2;
+
 window.addEventListener("keydown", (e) => {
   if (e.code === 'Space') {
     anime = !anime;
     updateIsPlayingLabel();
+  }
+
+  if (e.code === 'ArrowUp') {
+    renderer2Size = 1 - renderer2Size;
+    renderer2.setSize(window.innerWidth * renderer2Size, window.innerWidth * renderer2Size * 1.6);
   }
 });
 
@@ -39,13 +50,15 @@ renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 const renderer2 = new THREE.WebGLRenderer();
-renderer2.setSize(window.innerWidth / 5, window.innerWidth / 5 * 1.6);
+renderer2.setSize(window.innerWidth * renderer2Size, window.innerWidth * renderer2Size * 1.6);
 renderer2.shadowMap.enabled = true;
 document.body.appendChild(renderer2.domElement);
 renderer2.domElement.classList.add('renderer2')
 
 function makeMan() {
   const material = new THREE.MeshPhongMaterial({color: 0x00ff00});
+  const material2 = new THREE.MeshPhongMaterial({color: 0x00aaff});
+  const material3 = new THREE.MeshPhongMaterial({color: 0x888888});
 
   const man = new THREE.Object3D();
 
@@ -57,6 +70,7 @@ function makeMan() {
   const body = new THREE.Mesh(new THREE.CapsuleGeometry(1, 2, 2, 8), material);
   body.position.y = -2.2;
   body.rotation.x = THREE.MathUtils.degToRad(-30);
+  body.scale.z = 0.7
   body.castShadow = true;
   man.add(body);
 
@@ -78,12 +92,10 @@ function makeMan() {
   arm2.castShadow = true;
   man.add(arm2);
 
-  // man.rotation.x = -0.5;
-
-  const wheelGeometry = new THREE.TorusGeometry(2, 0.4, 8, 24);
+  const wheelGeometry = new THREE.TorusGeometry(2, 0.2, 8, 24);
   const wheel1 = new THREE.Mesh(
     wheelGeometry,
-    material
+    material3
   );
   wheel1.rotation.y = THREE.MathUtils.degToRad(90);
   wheel1.position.set(0, -6, -4);
@@ -92,12 +104,54 @@ function makeMan() {
 
   const wheel2 = new THREE.Mesh(
     wheelGeometry,
-    material
+    material3
   );
   wheel2.rotation.y = THREE.MathUtils.degToRad(90);
-  wheel2.position.set(0, -6, 4);
+  wheel2.position.set(0, -6, 3);
   wheel2.castShadow = true;
   man.add(wheel2);
+
+  let fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 3.2, 8), material2);
+  fork.position.set(0.5, -4.7, -3.5);
+  fork.rotation.x = THREE.MathUtils.degToRad(20);
+  fork.castShadow = true;
+  man.add(fork);
+  fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 3.2, 8), material2);
+  fork.position.set(-0.5, -4.7, -3.5);
+  fork.rotation.x = THREE.MathUtils.degToRad(20);
+  fork.castShadow = true;
+  man.add(fork);
+  fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 4, 8), material2);
+  fork.position.set(0, -4.5, -1.5);
+  fork.rotation.x = THREE.MathUtils.degToRad(-40);
+  fork.castShadow = true;
+  man.add(fork);
+  fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 3, 8), material2);
+  fork.position.set(0, -3.6, -1);
+  fork.rotation.x = THREE.MathUtils.degToRad(-75);
+  fork.castShadow = true;
+  man.add(fork);
+  fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 3.5, 8), material2);
+  fork.position.set(0, -5, 1.8);
+  fork.rotation.x = THREE.MathUtils.degToRad(-55);
+  fork.castShadow = true;
+  man.add(fork);
+  fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 4, 8), material2);
+  fork.position.set(0, -4.2, 0.2);
+  fork.rotation.x = THREE.MathUtils.degToRad(10);
+  fork.castShadow = true;
+  man.add(fork);
+  fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 3.5, 8), material2);
+  fork.position.set(0, -6.1, 1.5);
+  fork.rotation.x = THREE.MathUtils.degToRad(90);
+  fork.castShadow = true;
+  man.add(fork);
+
+  fork = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 4, 8), material2);
+  fork.position.set(0, -2.5, -2.9);
+  fork.rotation.z = THREE.MathUtils.degToRad(90);
+  fork.castShadow = true;
+  man.add(fork);
 
   return man
 }
@@ -112,19 +166,32 @@ function makeMan() {
 // }
 
 const man = makeMan();
-man.position.z = 50;
+man.position.z = manInitZ;
 scene.add(man);
+
+// =======================================================
+// 原点
+// 赤X 緑Y 青Z
+// scene.add(new THREE.AxesHelper(5));
 
 // =======================================================
 // 地面
 const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(50, roadSize * 3),
+  new THREE.PlaneGeometry(50, roadSize * roadTileNum),
   new THREE.MeshPhongMaterial({color: 0xaaccff})
 );
+// 長さの半分ずらして、端を原点にする
+floor.geometry.translate( 0, roadSize * roadTileNum / 2, 0 );
+
 floor.receiveShadow = true;
-floor.position.set(0, -8, -roadSize);
+floor.position.set(0, -8, 0);
 floor.rotation.x = THREE.MathUtils.degToRad(-90);
 scene.add(floor)
+
+// const box = new THREE.BoxHelper( floor, 0xffff00 );
+// scene.add( box );
+// const floorAixsHelper = new THREE.AxesHelper(5);
+// floor.add(floorAixsHelper);
 
 // =======================================================
 // ライト
@@ -143,11 +210,12 @@ scene.add(floor)
 // const lightHelper = new THREE.SpotLightHelper(light2);
 // scene.add(lightHelper);
 
+// ライト列の生成と配置
 const sphereSize = 5;
 const lightMeshMat = new THREE.MeshBasicMaterial({color: 0xcccccc});
-for (let i = -1; i < 5; i++) {
+for (let i = 0; i <= 7; i++) {
   let light = new THREE.PointLight(0xff3333, 1, 200, 2);
-  light.position.set(20, 20, 0 - i * 150);
+  light.position.set(20, 20, 0 - i * (roadSize / 2));
   light.castShadow = true;
   scene.add(light);
 
@@ -157,13 +225,17 @@ for (let i = -1; i < 5; i++) {
 
   // scene.add(new THREE.PointLightHelper(light, sphereSize));
 }
+
+// scene.add( new THREE.AmbientLight( 0x222222 ) );
+
 // =======================================================
 // カメラのポジション
 const cameraOffsetZ = 10;
 
 // 俯瞰カメラ
-// camera1.position.set(-50, 20, 0); // 横から
+// camera1.position.set(-20, 0, 0); // 横から
 camera1.position.set(0, 200, 0); // 上から
+// camera2.rotation.x = THREE.MathUtils.degToRad(-90);
 
 // カメラコントローラーを作成
 const controls = new THREE.OrbitControls(camera1, document.body);
@@ -185,7 +257,7 @@ function animate() {
     camera2.position.z += -0.1;
     man.position.z += -1;
     if (man.position.z < -roadSize / 2) {
-      man.position.z = roadSize / 2;
+      man.position.z = manInitZ;
     }
     camera2.position.z = man.position.z + cameraOffsetZ;
   }
